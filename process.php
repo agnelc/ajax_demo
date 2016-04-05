738faf3365a2625232fe78ab2a60a8eeec43a562
AGNEL THOMAS C <agnelcagnel@gmail.com>
2016-04-05 23:04:25 +0530

    .

--- process.php
+++ process.php
@@ -1,6 +1,8 @@
 <?php 
 include("db.php");
 
+
+/***********Displaying Action Box Data**************************/   
 if(isset($_POST['id'])){
 
     $id = mysqli_real_escape_string($connection,$_POST['id']);
@@ -13,21 +15,68 @@ if(isset($_POST['id'])){
     
     while($row = mysqli_fetch_array($query_car_info)){
       
-        echo "<input type='text' class='form-control car_id' value='".$row['title']."'>";
-        echo "<input type='button' class='btn btn-success btn-block car_id' value='Update'>";
-        echo "<input type='button' class='btn btn-danger btn-block car_id' value='Delete'>";
+        echo "<input rel='".$row['id']."' type='text' class='form-control title_input' value='".$row['title']."'>";
+        echo "<input type='button' class='btn btn-success btn-block update' value='Update'>";
+        echo "<input type='button' class='btn btn-danger btn-block delete' value='Delete'>";
         
     }
     
 }
 
+/*************Updating Data**************************************/   
 
-?>
+if(isset($_POST['updatethis'])){
+    
+    $id = mysqli_real_escape_string($connection,$_POST['id']);
+    $title = mysqli_real_escape_string($connection,$_POST['title']);
+    
+    $query = "UPDATE cars SET title = '$title' WHERE id = '$id'";
+    
+    
+    
+}
+
+/*************EOF Updating Data**************************************/  
+
+
+
+?> 
+<!--************EOF php tagData**************************************/-->  
+
+<!--******************Script**************************************-->  
  <script>
-            $(document).ready(function(){
+    $(document).ready(function(){
+
+        var id;
+        var title;
+        var updatethis = "update";
+        var deletethis = "delete";
                 
-                var id;
-                var title;
+/***********Extract id and title**************************/
+
+        $(".title_input").on('input',function(){
+            id = $(this).attr('rel');
+            title = $(this).val();
+        
+        });
+  
+/***********Update  Button functionality**************************/          
+            
+        $(".update").on('click',function(){
+
+            $.post("process.php",{id:id,title:title,updatethis:updatethis},function(data){
+               
+               alert(data);
+               
+            });
+
+        });
+            
+            
+        
+                
+                
+                
                
     });
 
