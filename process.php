1ceb025ab41b35aa36414b942f9d838f6fadd675
AGNEL THOMAS C <agnelcagnel@gmail.com>
2016-04-06 11:51:32 +0530

    .

--- process.php
+++ process.php
@@ -15,9 +15,11 @@ if(isset($_POST['id'])){
     
     while($row = mysqli_fetch_array($query_car_info)){
       
+        echo "<p id='feedback' class='bg-success'></p>";
         echo "<input rel='".$row['id']."' type='text' class='form-control title_input' value='".$row['title']."'>";
         echo "<input type='button' class='btn btn-success btn-block update' value='Update'>";
         echo "<input type='button' class='btn btn-danger btn-block delete' value='Delete'>";
+        echo "<input type='button' class='btn btn-close' value='Close'>";
         
     }
     
@@ -31,12 +33,33 @@ if(isset($_POST['updatethis'])){
     $title = mysqli_real_escape_string($connection,$_POST['title']);
     
     $query = "UPDATE cars SET title = '$title' WHERE id = '$id'";
-    
+    $result_set = mysqli_query($connection, $query);
+    if(!$result_set)
+    {
+        die("Query Failed" . mysqli_error($connection));
+    }
     
     
 }
 
 /*************EOF Updating Data**************************************/  
+/*************Deleting Data**************************************/   
+
+if(isset($_POST['deletethis'])){
+    
+    $id = mysqli_real_escape_string($connection,$_POST['id']);
+    
+    $query = "DELETE FROM cars WHERE id = '$id'";
+    $result_set = mysqli_query($connection, $query);
+    if(!$result_set)
+    {
+        die("Query Failed" . mysqli_error($connection));
+    }
+    
+    
+}
+
+/*************EOF Deleting Data**************************************/  
 
 
 
@@ -66,14 +89,37 @@ if(isset($_POST['updatethis'])){
 
             $.post("process.php",{id:id,title:title,updatethis:updatethis},function(data){
                
-               alert(data);
+               $("#feedback").html("Record updated successfully");
                
             });
 
         });
+/***********Delete  Button functionality**************************/          
             
+        $(".delete").on('click',function(){
+
+            if(confirm("Are you sure you want to delete this")){
+                
             
-        
+            id = $(".title-link").attr('rel');
+            $.post("process.php",{id:id,deletethis:deletethis},function(data){
+               
+              alert("Record deleted successfully");
+              $("#action-container").hide();
+               
+            });
+            }
+
+        });          
+         
+
+/***********Close  Button functionality**************************/          
+        $(".btn-close").on('click',function(){
+
+              $("#action-container").hide();
+               
+
+        });  
                 
                 
                 
